c69a3de90bacbabd45fc8418a2fbedb2c1ea679c
Shane Loretz <shane.loretz@gmail.com>
2016-09-14 17:39:24 -0700

    Added insert method to singly linked list

--- datastructures/linked_list.py
+++ datastructures/linked_list.py
@@ -37,6 +37,29 @@ class SinglyLinkedList(object):
             self._last.link(n)
         self._last = n
 
+    def insert(self, idx, item):
+        """Inserts item at position idx in the list"""
+        if idx > self._num or idx < 0:
+            raise IndexError()
+
+        self._num += 1
+        new_node = self.Node(item)
+
+        if 0 == idx:
+            new_node.link(self._root)
+            self._root = new_node
+        elif self._num > 0:
+            # find the node before where this will be inserted
+            node = self._root
+            while idx > 1:
+                node = node._next
+                idx -= 1
+            new_node.link(node._next)
+            node.link(new_node)
+
+        if new_node._next is None:
+            self._last = new_node
+
     def __getitem__(self, idx):
         """Return an item at the given index"""
         idx = int(idx)
@@ -142,6 +165,52 @@ class TestSinglyLinkedList(unittest.TestCase):
         [lis.__delitem__(len(lis)-1) for lis in (truth, l)]
         for idx in range(len(l)):
             self.assertEqual(truth[idx], l[idx])
+
+    def test_insert_first(self):
+        l = SinglyLinkedList()
+        l.insert(0, 1)
+        l.insert(0, 2)
+        l.insert(0, 3)
+        self.assertEqual(3, len(l))
+        self.assertEqual(3, l[0])
+        self.assertEqual(2, l[1])
+        self.assertEqual(1, l[2])
+
+    def test_insert_last(self):
+        l = SinglyLinkedList()
+        l.append(1)
+        l.insert(1, 2)
+        self.assertEqual(2, len(l))
+        self.assertEqual(1, l[0])
+        self.assertEqual(2, l[1])
+
+    def test_insert_beyond_last(self):
+        l = SinglyLinkedList()
+        l.append(1)
+        with self.assertRaises(IndexError):
+            l.insert(2, 4)
+
+    def test_del_append_insert_a_lot(self):
+        truth = []
+        l = SinglyLinkedList()
+        [lis.append(1) for lis in (truth, l)]
+        for idx in range(len(l)):
+            self.assertEqual(truth[idx], l[idx])
+        [lis.append(4) for lis in (truth, l)]
+        [lis.append(3) for lis in (truth, l)]
+        [lis.__delitem__(2) for lis in (truth, l)]
+        [lis.insert(0,5) for lis in (truth, l)]
+        [lis.insert(len(l), 9) for lis in (truth, l)]
+        [lis.__delitem__(2) for lis in (truth, l)]
+        [lis.__delitem__(0) for lis in (truth, l)]
+        [lis.append(6) for lis in (truth, l)]
+        [lis.__delitem__(0) for lis in (truth, l)]
+        [lis.insert(2,7) for lis in (truth, l)]
+        [lis.append(8) for lis in (truth, l)]
+        [lis.__delitem__(len(lis)-1) for lis in (truth, l)]
+        for idx in range(len(l)):
+            self.assertEqual(truth[idx], l[idx])
+
 
 if __name__ == '__main__':
     unittest.main()
